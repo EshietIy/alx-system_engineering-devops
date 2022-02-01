61d292b6c181de3d77a43f26e925d94b60ea1fd3
EshietIy <eshietinyang@gmail.com>
2022-02-01 21:16:09 +0100

    a Python script that, using this REST API, for a given employee ID

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,3 +1,15 @@
+#!/usr/bin/python3
+'''A script that gathers data from an API.
+'''
+import re
+import requests
+import sys
+
+
+API_URL = 'https://jsonplaceholder.typicode.com'
+'''The API's URL.'''
+
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
