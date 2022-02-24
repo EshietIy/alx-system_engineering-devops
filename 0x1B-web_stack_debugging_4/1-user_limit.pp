ef3f5e516d27856c46a8216975f99670bccca2b4
EshietIy <eshietinyang@gmail.com>
2022-02-25 00:08:47 +0100

    fixed

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,8 +1,11 @@
-# Changes the limitations on the holberton user
-exec { 'change-os-configuration-for-holberton-user':
-  command => "bash -c \"sed -iE 's/^holberton hard nofile \
-5/holberton hard nofile 88888/' /etc/security/limits.conf; \
-sed -iE 's/^holberton soft nofile \
-4/holberton soft nofile 88888/' /etc/security/limits.conf\"",
-  path    => '/usr/bin:/usr/sbin:/bin'
+# change the limit of open files in os
+exec {'change limit soft':
+  command  => "sudo sed -i 's/holberton soft nofile 4/holberton soft nofile 88888/g'  /etc/security/limits.conf",
+  provider => shell
+}
+
+# change the limit of open files in  os
+exec {'change limit hard':
+  command  => "sudo sed -i 's/holberton hard nofile 5/holberton hard nofile 88888/g'  /etc/security/limits.conf",
+  provider => shell
 }
